0b06160593b79cef3a2aeaf401ff79db4aaa8b4d
Laila Silva <laila.cristine@outlook.com>
2024-07-25 22:19:29 -0300

    criando secao hero

---
+++ src/app/components/hero.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Hero() {
+  return (
+    <div>
+      <h1 className="text-center text-white font-bold text-8xl w-5/12 flex justify-center items-center mx-auto pt-20">
+      Inovando os cuidados da saúde</h1>
+      <p className="text-green font-bold text-center">Simples. Essencial. Digital.</p>
+    </div>
+  )
+}
