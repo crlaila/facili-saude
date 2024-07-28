129ab7ddafd80452a41ef1c9008f3fa05b4b830b
Laila Silva <laila.cristine@outlook.com>
2024-07-27 23:25:25 -0300

    ajustes na secao how

--- src/app/components/how.js
+++ src/app/components/how.js
@@ -10,19 +10,19 @@ export default function How() {
       </div>
       <div className="max-w-3xl mx-auto mt-16 flex gap-14">
         <div className="flex flex-col items-center justify-center text-gray 
-        py-10 px-7 w-56 rounded-3xl bg-white drop-shadow-custom">
+        py-10 px-7 w-56 rounded-3xl bg-white drop-shadow-custom border-white border-2 box-hover">
           <Image  src="/contract.svg" alt="Contrato" width={36} height={36} className="mx-auto"/>
           <h3 className="font-bold py-3">Contrato</h3>
           <p className="text-xs">Pra ter o seu desconto basta selecionar o plano que mais combina com você e assinar o seu contrato digital.</p>
         </div>
         <div className="flex flex-col items-center justify-center text-gray 
-        py-10 px-7 w-56 rounded-3xl bg-white drop-shadow-custom">
+        py-10 px-7 w-56 rounded-3xl bg-white drop-shadow-custom border-white border-2 box-hover">
           <Image  src="/active.svg" alt="Ativação" width={22} height={36} className="mx-auto"/>
           <h3 className="font-bold py-3">Ative</h3>
           <p className="text-xs">Pronto! Agora que você já assinou o seu contrato você passa a fazer parte de diversos benefícios.</p>
         </div>
         <div className="flex flex-col items-center justify-center text-gray 
-        py-10 px-7 w-56 rounded-3xl bg-white drop-shadow-custom">
+        py-10 px-7 w-56 rounded-3xl bg-white drop-shadow-custom border-white border-2 box-hover">
           <Image  src="/use.svg" alt="Use" width={18} height={36} className="mx-auto"/>
           <h3 className="font-bold py-3">Use</h3>
           <p className="text-xs">Agora sim! :) <br/>Após a ativação você já está liberado para aproveitar todos os seus descontos.</p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,30 +6,42 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-      colors: {
-        'green': '#53F95F',
-        'blue': '#4263FF',
-        'gray': '#4B4B4B',
-        'purple': '#721EFF',
-        'white': '#FFFFFF',
-        transparent: 'transparent',
-        'bg-hero':'radial-gradient(28.9% 28.9% at 50% 67.59%, #FFFFFF 0%, #A5B5FF 24.34%, #4263FF 88%)',
-        
-      },
-      screens: {
-        sm: '480px',
-        md: '768px',
-        lg: '976px',
-        xl: '1440px',
-      },
-      extend: {
-        fontSize: {
-          'sub': '35px',
-        },
-        dropShadow: {
-          'custom': '0px 0px 20px rgba(0, 0, 0, 0.10)',
-        },
-      },
+    colors: {
+      'green': '#53F95F',
+      'blue': '#4263FF',
+      'gray': '#4B4B4B',
+      'purple': '#721EFF',
+      'white': '#FFFFFF',
+      transparent: 'transparent',
+      'bg-hero':'radial-gradient(28.9% 28.9% at 50% 67.59%, #FFFFFF 0%, #A5B5FF 24.34%, #4263FF 88%)',
       
-    },  
+    },
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      fontSize: {
+        'sub': '35px',
+      },
+      dropShadow: {
+        'custom': '0px 0px 20px rgba(0, 0, 0, 0.10)',
+      },
+    },
+  },
+  plugins: [
+    function({ addUtilities }) {
+      addUtilities({
+        '.box-hover': {
+          transition: 'all 0.5s ease-in-out',
+          '&:hover': {
+            borderColor: '#3b82f6',
+            borderWidth: '2px',
+          },
+        },
+      }, ['responsive', 'hover']);
+    },
+  ],
 };
